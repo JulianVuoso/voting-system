388e7f6488a5edfaaeecfbec71d67cbf806197a0
Julian Tallar <jtallar@itba.edu.ar>
2020-09-07 17:25:32 -0300

    Solved TODO

--- api/src/main/java/ar/edu/itba/pod/tpe/interfaces/VoteAvailableCallbackHandler.java
+++ api/src/main/java/ar/edu/itba/pod/tpe/interfaces/VoteAvailableCallbackHandler.java
@@ -8,8 +8,11 @@ public interface VoteAvailableCallbackHandler extends Remote {
      * Method to be called when a vote is registered if appropriate
      * @throws RemoteException if any communication error occurs.
      */
-    // TODO: Este metodo recibe el Vote? Como que capaz tiene info de mas
     void voteRegistered() throws RemoteException;
 
+    /**
+     * Method to be called when the election finishes
+     * @throws RemoteException if any communication error occurs.
+     */
     void electionFinished() throws RemoteException;
 }
