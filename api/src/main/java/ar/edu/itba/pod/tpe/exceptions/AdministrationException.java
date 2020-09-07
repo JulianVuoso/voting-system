060d8e2a965e9b1c0eae587d074cc53f128c89dd
Matias Brula <mbrula@itba.edu.ar>
2020-09-07 14:55:04 -0300

    admin service - complete admin exception class

--- api/src/main/java/ar/edu/itba/pod/tpe/exceptions/AdministrationException.java
+++ api/src/main/java/ar/edu/itba/pod/tpe/exceptions/AdministrationException.java
@@ -1,11 +1,26 @@
 package ar.edu.itba.pod.tpe.exceptions;
 
-import java.security.PublicKey;
-
 public class AdministrationException extends Exception {
+
+    private static final long serialVersionUID = 1533564635752790375L;
 
     public AdministrationException() {
         super();
     }
 
+    public AdministrationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public AdministrationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AdministrationException(String message) {
+        super(message);
+    }
+
+    public AdministrationException(Throwable cause) {
+        super(cause);
+    }
 }
