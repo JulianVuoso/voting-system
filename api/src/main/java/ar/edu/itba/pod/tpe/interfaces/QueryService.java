50a4338bf8623f8c8aadf4abd022b68140804436
JulianVuoso <juvuoso@itba.edu.ar>
2020-09-08 10:54:22 -0300

    configured interface

--- api/src/main/java/ar/edu/itba/pod/tpe/interfaces/QueryService.java
+++ api/src/main/java/ar/edu/itba/pod/tpe/interfaces/QueryService.java
@@ -1,6 +1,27 @@
 package ar.edu.itba.pod.tpe.interfaces;
 
+import ar.edu.itba.pod.tpe.exceptions.QueryException;
+import com.sun.jndi.cosnaming.RemoteToCorba;
+
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface QueryService extends Remote {
+
+    /**
+     * Ask for partial/final results respectively for different dimensions
+     * Export .csv for each query and print on stdout
+     * If polls open -> partial with FPTP
+     * If polls finished -> final with correspondent system
+     * @throws QueryException if ask with polls closed.
+     * @throws RemoteException if any communication error occurs.
+     */
+    void ask_national() throws RemoteException, QueryException;
+    void ask_provincial(String state) throws RemoteException, QueryException;
+    void ask_table(Integer table) throws RemoteException, QueryException;
 }
+
+/*
+Cliente sólo pregunta lo que quiere e imprime en pantalla el resultado + exporta csv con resultados de la consulta.
+
+* */
--- server/src/main/java/ar/edu/itba/pod/tpe/server/QueryServiceImpl.java
+++ server/src/main/java/ar/edu/itba/pod/tpe/server/QueryServiceImpl.java
@@ -1,6 +1,24 @@
 package ar.edu.itba.pod.tpe.server;
 
+import ar.edu.itba.pod.tpe.exceptions.QueryException;
 import ar.edu.itba.pod.tpe.interfaces.QueryService;
 
+import java.rmi.RemoteException;
+
 public class QueryServiceImpl implements QueryService {
+
+    @Override
+    public void ask_national() throws RemoteException, QueryException {
+
+    }
+
+    @Override
+    public void ask_provincial(String state) throws RemoteException, QueryException {
+
+    }
+
+    @Override
+    public void ask_table(Integer table) throws RemoteException, QueryException {
+
+    }
 }
