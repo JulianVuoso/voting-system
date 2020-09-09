0daa2dfc8d4259a0a89733eb93bf5044ecea2763
Matias Brula <mbrula@itba.edu.ar>
2020-09-09 09:21:55 -0300

    admin server - server main

--- server/src/main/java/ar/edu/itba/pod/tpe/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/tpe/server/Server.java
@@ -1,12 +1,26 @@
 package ar.edu.itba.pod.tpe.server;
 
+import ar.edu.itba.pod.tpe.interfaces.AdministrationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 public class Server {
     private static Logger logger = LoggerFactory.getLogger(Server.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RemoteException {
         logger.info("tpe1-g6 Server Starting ...");
+
+        final AdministrationService gs = new AdministrationServiceImpl();
+
+        final Remote remote = UnicastRemoteObject.exportObject(gs, 0);
+
+        final Registry registry = LocateRegistry.getRegistry();
+        registry.rebind("dummy", remote);
     }
 }
