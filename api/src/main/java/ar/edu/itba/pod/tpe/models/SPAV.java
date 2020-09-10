83d276dd8109c4017322aeb232141ac6be3a6a22
JulianVuoso <juvuoso@itba.edu.ar>
2020-09-10 15:41:57 -0300

    SPAV query

--- api/src/main/java/ar/edu/itba/pod/tpe/models/SPAV.java
+++ api/src/main/java/ar/edu/itba/pod/tpe/models/SPAV.java
@@ -1,5 +1,18 @@
 package ar.edu.itba.pod.tpe.models;
 
-public class SPAV extends Result {
+import java.util.Map;
 
+public class SPAV extends Result {
+    Map<String, Double> round1;
+    Map<String, Double> round2;
+    Map<String, Double> round3;
+
+    public SPAV(Map<String, Double> round1, Map<String, Double> round2, Map<String, Double> round3, String[] winners) {
+        this.round1 = round1;
+        this.round2 = round2;
+        this.round3 = round3;
+        this.winner = winners;
+        this.partial = false;
+        this.type = Type.SPAV;
+    }
 }
--- server/src/main/java/ar/edu/itba/pod/tpe/server/QueryServiceImpl.java
+++ server/src/main/java/ar/edu/itba/pod/tpe/server/QueryServiceImpl.java
@@ -41,11 +41,35 @@ public class QueryServiceImpl implements QueryService {
         if(status==0) throw new QueryException("Polls already closed");
         if(status==1)                                          //  open
             return stateFptp.get(state);
-//        stateSpav.put()                                             //  finished
-        // en cada key va a haber una provincia
-        // que en cada valor va a tener un SPAV que contiene 3 mapas
-        return null;
+
+        String[] winners = new String[3];
+        Map<String, Double> round1 = spavIterator(state, null);
+        winners[0] = Collections.max(round1.entrySet(), Comparator.comparingDouble(Map.Entry::getValue)).getKey();
+        Map<String, Double> round2 = spavIterator(state, winners);
+        winners[1] = Collections.max(round2.entrySet(), Comparator.comparingDouble(Map.Entry::getValue)).getKey();
+        Map<String, Double> round3 = spavIterator(state, winners);
+        winners[2] = Collections.max(round3.entrySet(), Comparator.comparingDouble(Map.Entry::getValue)).getKey();
+
+        return new SPAV(round1, round2, round3, winners);
     }
+
+    private Map<String, Double> spavIterator(String state, String[] winners){
+        List<Vote> stateVotes = new ArrayList<>();
+        for(List<Vote> list : votes.get(state).values())        // me quedo con todos los votos del state
+            stateVotes.addAll(list);
+
+        Map<String, Double> spavRound = new HashMap<>();
+        for(Vote vote : stateVotes){                                // por cada voto
+            Map<String, Double> mapVote = vote.getSPAV(winners);    // obtengo su party -> puntaje
+            Set<String> parties = mapVote.keySet();                 // y por cada party
+            for(String party : parties){
+                spavRound.putIfAbsent(party, 0.0);                  // le sumo al map general tal puntaje
+                spavRound.put(party, spavRound.get(party) + mapVote.get(party));
+            }
+        }
+        return spavRound;
+    }
+
 
     @Override
     public Result askTable(Integer table) throws RemoteException, QueryException {
@@ -66,23 +90,28 @@ public class QueryServiceImpl implements QueryService {
         natFptp.getMap().put(vote.getVoteFPTP(), natFptp.getMap().get(vote.getVoteFPTP()+1));
 
         stateFptp.putIfAbsent(vote.getState(), new FPTP());                                     // STATE: si es el primer voto de esa provincia le agrego un FPTP
-        stateFptp.get(vote.getState()).getMap().put(vote.getVoteFPTP(), stateFptp.get(vote.getState()).getMap().get(vote.getVoteFPTP()+1)); // Luego obtengo ese FPTP y le meto en key Party 1 voto mas
-
+        stateFptp.get(vote.getState()).getMap().put(vote.getVoteFPTP(), stateFptp.get(vote.getState()).getMap().get(vote.getVoteFPTP()+1));
+                                                                                                // Luego obtengo ese FPTP y le meto en key Party 1 voto mas
         tableFptp.putIfAbsent(vote.getTable(), new FPTP());                                     // TABLE: same a state
         tableFptp.get(vote.getTable()).getMap().put(vote.getVoteFPTP(), tableFptp.get(vote.getTable()).getMap().get(vote.getVoteFPTP()+1));
     }
 
-    private Map<String, Integer> firstSTAR() {
+    private List<Vote> allVotes(){
         List<Vote> totalVotes = new ArrayList<>();
-        Map<String, Integer> firstStar = new HashMap<>();
         for(Map<Integer, List<Vote>> vote : votes.values()){
             for(List<Vote> list : vote.values())
                 totalVotes.addAll(list);
         }
-        for(Vote vote : totalVotes){
-            for(String party : vote.getParty().keySet()){
+        return totalVotes;
+    }
+
+    private Map<String, Integer> firstSTAR() {
+//        List<Vote> totalVotes = allVotes();
+        Map<String, Integer> firstStar = new HashMap<>();
+        for(Vote vote : allVotes()){
+            for(String party : vote.getSPAV(null).keySet()){
                 firstStar.putIfAbsent(party, 0);
-                firstStar.putIfAbsent(party, firstStar.get(party) + vote.getParty().get(party));
+                firstStar.put(party, firstStar.get(party) + vote.getSPAV(null).get(party).intValue());
             }
         }
         return firstStar;
@@ -91,60 +120,40 @@ public class QueryServiceImpl implements QueryService {
     private Map<String, Double> secondSTAR() {
         Map<String, Integer> aux = natFptp.getMap();                // TODO: ver una forma mejor
         Map<String, Double> secondStar = new HashMap<>();
+        Map<String, Integer> points = new HashMap<>();
+
         String winner1 = Collections.max(aux.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
         aux.put(winner1, -1);
         String winner2 = Collections.max(aux.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
 
-        int total = aux.get(winner1) + aux.get(winner2);
-        secondStar.put(winner1, aux.get(winner1).doubleValue() / total * 100);
-        secondStar.put(winner2, 100 - aux.get(winner1).doubleValue());
         // winner 1 -> %
         // winner 2 -> %
+        String winnerAlpha = winner1.compareTo(winner2)<0 ? winner1:winner2;
+        for(Vote vote : allVotes()){
+            if(vote.getSPAV(null).get(winner1)>vote.getSPAV(null).get(winner2)){    // si en el voto w1 > w2
+                points.putIfAbsent(winner1, 0);                               // le sumo uno a w1 en el map
+                points.put(winner1, points.get(winner1)+1);
+            }
+            else{
+                if(vote.getSPAV(null).get(winner1)<vote.getSPAV(null).get(winner2)){
+                    points.putIfAbsent(winner2, 0);
+                    points.put(winner2, points.get(winner2)+1);
+                }
+                else{
+                    points.putIfAbsent(winnerAlpha, 0);                       // si son iguales se lo sumo
+                    points.put(winnerAlpha, points.get(winnerAlpha)+1);       // al menor alfabeticamente
+                }
+            }
+        }
+        int total = points.get(winner1) + points.get(winner2);
+        secondStar.put(winner1, points.get(winner1).doubleValue() / total * 100);
+        secondStar.put(winner2, 100 - points.get(winner1).doubleValue());
 
         return secondStar;
     }
-
 }
-
-/*
-Map<String, Integer> getFinalNational();
-Map<String, Double> getFinalState(String state);
-Map<String, Double> getTable(Integer table);
-Map<String, Double> getPartialNational();
-Map<String, Double> getPartialState(String state);
-* */
-
 
   /*  for(Map<Integer, List<Vote>> vote : tabledVotes){
         for(List<Vote> list : vote.values())
             totalVotes.addAll(list);
-    }*/
-
-
-
-
- /*   private Result finalTable(Integer table) throws RemoteException, QueryException{
-        Map<String, Double> fptp2 = new HashMap<>();
-        Map<String, Integer> fptpVotes = new HashMap<>();
-        List<Vote> totalVotes = new ArrayList<>();
-        int total = 0;
-
-        for(Map<Integer, List<Vote>> vote : votes.values())
-            totalVotes.addAll(vote.get(table));
-
-        for(Vote vote : totalVotes){
-            fptpVotes.putIfAbsent(vote.getVoteFPTP(),0);
-            fptpVotes.put(vote.getVoteFPTP(), fptpVotes.get(vote.getVoteFPTP())+1);
-            total++;
-        }
-
-        for(String party : fptpVotes.keySet()){
-            fptp2.put(party, fptpVotes.get(party).doubleValue() / total * 100);
-        }
-
-        return new FPTP(fptp2, true, Type.FPTP);
-    }
-
-    private Map<String, Integer> partialFtpt(Integer table){
-
     }*/
