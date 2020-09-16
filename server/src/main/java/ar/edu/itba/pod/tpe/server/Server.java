8556e36f26bbba3beb637538b3a16045b43234a9
Julian Tallar <jtallar@itba.edu.ar>
2020-09-15 21:52:57 -0300

    Capture server exceptions to display custom error message

--- server/src/main/java/ar/edu/itba/pod/tpe/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/tpe/server/Server.java
@@ -16,17 +16,23 @@ import java.rmi.server.UnicastRemoteObject;
 public class Server {
     private static Logger logger = LoggerFactory.getLogger(Server.class);
 
-    public static void main(String[] args) throws RemoteException {
-        logger.info("tpe1-g6 Server Starting ...");
+    private static final int ERROR_STATUS = 1;
 
-        final Remote remote = UnicastRemoteObject.exportObject(new ElectionServiceImpl(), 0);
+    public static void main(String[] args) {
+        try {
+            final Remote remote = UnicastRemoteObject.exportObject(new ElectionServiceImpl(), 0);
 
-        final Registry registry = LocateRegistry.getRegistry();
-        registry.rebind(InspectionService.class.getName(), remote);
-        registry.rebind(ManagementService.class.getName(), remote);
-        registry.rebind(VotingService.class.getName(), remote);
-        registry.rebind(QueryService.class.getName(), remote);
+            final Registry registry = LocateRegistry.getRegistry();
+            registry.rebind(InspectionService.class.getName(), remote);
+            registry.rebind(ManagementService.class.getName(), remote);
+            registry.rebind(VotingService.class.getName(), remote);
+            registry.rebind(QueryService.class.getName(), remote);
+        } catch (RemoteException e) {
+            System.err.println("Remote communication with registry failed.");
+            System.exit(ERROR_STATUS);
+            return;
+        }
 
-        logger.info("Election Service bound");
+        System.out.println("Election Service bound");
     }
 }
